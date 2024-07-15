d68804a938f7a62ae5a852fc93a2fe19081a06c7
Ariel dos Santos Nunes <arielsn1@gmail.com>
2024-07-15 17:02:04 -0300

    feat: added configs, tailwind, testing and first page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,76 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+            board: {
+                name: "Projeto 01",
+                columns: [
+                    {
+                        name: 'Todo',
+                        cards: [
+                            { title: 'A' },
+                            { title: 'B' },
+                            { title: 'C' }
+                        ]
+                    },
+                    {
+                        name: 'Doing',
+                        cards: []
+                    },
+                    {
+                        name: 'Done',
+                        cards: []
+                    }
+                ]
+            }
+        }
+    }
+}
+</script>
+
 <template>
-    <div>
-        <h3 class="">Hello</h3>
+    <h3 class="font-bold text-3xl">
+        {{ board.name }}
+    </h3>
+    <div class="columns">
+        <div v-for="column in board.columns" class="column">
+            <h4 class="font-bold text-2xl">{{ column.name }}</h4>
+            <div v-for="card in column.cards" class="card">
+                {{ card.title }}
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.columns {
+    @apply flex;
+    @apply flex-row;
+}
+
+.column {
+    @apply w-52;
+    @apply text-center;
+    @apply bg-gray-400;
+    @apply mr-1;
+    @apply p-3;
+    @apply border;
+    @apply border-black;
+    @apply border-solid;
+
+}
+
+.card {
+    @apply flex;
+    @apply items-center;
+    @apply justify-center;
+    @apply text-center;
+    @apply bg-amber-300;
+    @apply h-20;
+    @apply border;
+    @apply border-black;
+    @apply border-solid;
+    @apply mb-3;
+    @apply w-full;
+}
+</style>
---
+++ test/test.test.ts
@@ -0,0 +1,6 @@
+import { expect, it, test } from "vitest";
+
+it('should work', function () {
+  expect(1).toBe(1);
+  expect(2).toBe(2);
+})
